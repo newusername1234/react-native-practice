e83246cf1184b7d260c7bb90c7dc7194d1f36558
Wes <wesleyayer92@gmail.com>
2020-02-26 12:52:04 -0500

    plays with flex size styles

--- App.js
+++ App.js
@@ -5,10 +5,11 @@ import { View } from 'react-native';
 export default class FixedDimensionsBasics extends Component {
   render() {
     return (
-      <View>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+      //parent with no fixed height and width or flex will have dimensions of 0 and children will not be visible
+      <View style={{flex: 1}}>
+        <View style={{flex: 3, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 1, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
